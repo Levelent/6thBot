e4bde1c17f1b727b1e96b947060f32863773935c
Levelent <31184681+Levelent@users.noreply.github.com>
2020-04-11 16:36:09 +0100

    fixed the gif command

--- cogs/fun.py
+++ cogs/fun.py
@@ -29,7 +29,7 @@ class Fun(commands.Cog):
     async def hug(self, ctx, target: Optional[Member] = None):
         # TODO Add hug counter
         you = ctx.author.display_name
-        if target is None:
+        if target is None or target == ctx.author:
             await ctx.send(f"**{you}** hugs themselves. It makes them feel a little better.")
             return
         await ctx.send(f"**{you}** hugs **{target.display_name}**. It's a little awkward, but they don't mind.")
@@ -46,18 +46,17 @@ class Fun(commands.Cog):
             return
         # Get from file (split by line), replace <you> and <them> with target.display_name and ctx.author.display_name
         line = choice(self.murder_lines)
-        line = line.replace("<you>", f"{you}")
-        line = line.replace("<user>", f"{target.display_name}")
+        line = line.replace("**<you>**", f"{you}")
+        line = line.replace("**<user>**", f"{target.display_name}")
         print(line)
         await ctx.send(line)
 
-    @commands.command()
     async def get_gif(self, tags=None):
-        url = f"http://api.giphy.com/v1/gifs/random?api_key={self.bot.giphy_api_key}&tag={tags or ''}"
+        url = f"http://api.giphy.com/v1/gifs/random?api_key={self.bot.giphy_api_key}&tag={tags}"
         return await get_json_content(url)
 
     @commands.command()
-    async def gif(self, ctx, *, search=None):
+    async def gif(self, ctx, *, search: str = ""):
         """Posts a random gif from the tags you enter.
 
         gif --> completely random gif.
@@ -72,14 +71,14 @@ class Fun(commands.Cog):
 
         data = content['data']
         if search is not None:
-            search = sub(" +", ", ", search)
+            search = sub(" +", ", ", search.strip())
 
-        em = Embed(color=0x8B008B, title=f"🔎 Tags: {search or '🎲'}", description=f"[Original]({data['image_url']})")
+        em = Embed(color=0xFA8072, title=f"🔎 Tags: {search or '🎲'}", description=f"[Original]({data['image_url']})")
         em.set_image(url=data['image_url'])
-        kilobytes = int(data['images']['original']['mp4_size'] // 1024)
+        kilobytes = int(data['images']['original']['mp4_size']) // 1024
         em.set_footer(text=f"Dimensions: {data['image_width']}x{data['image_height']} "
                            f"| Frames: {data['image_frames']} | Size: {kilobytes}kb")
-        em.set_author(name="Gif Search", icon_url=ctx.author.avatar_url)
+        em.set_author(name=f"Requested by {str(ctx.author)}", icon_url=ctx.author.avatar_url)
 
         await ctx.channel.send(embed=em)
 
