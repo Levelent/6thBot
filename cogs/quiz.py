cda513f2859ed14e0ddc34622729873a204c6e80
Levelent <31184681+Levelent@users.noreply.github.com>
2020-04-14 13:16:58 +0100

    quiz module almost completely re-written from scratch - now hides user answers, has streak system and allows for further improvements in the future

--- cogs/quiz.py
+++ cogs/quiz.py
@@ -1,6 +1,6 @@
 # Uses the OpenTDB API to fetch questions for a multi-round server quiz.
 from discord.ext import commands
-from discord import Embed
+from discord import Embed, PermissionOverwrite, Reaction, Message
 from asyncio import sleep
 from random import shuffle
 from json import loads
@@ -8,163 +8,265 @@ from html import unescape
 from aiohttp import ClientSession
 
 
+async def get_json_content(url):
+    async with ClientSession() as session:
+        async with session.get(url) as resp:
+            print("Response:" + str(resp.status))
+            json_string = await resp.text()
+    return loads(json_string)
+
+
+async def get_questions(difficulty, amount):
+    url = f"https://opentdb.com/api.php?amount={amount}&type=multiple&difficulty={difficulty}"
+    data = await get_json_content(url)
+    return [quest for quest in data['results']]
+
+
+class ScoreData:
+    def __init__(self):
+        # {"[member_id]": {"answered": 7, "correct": 5, "curr_streak": 2, "max_streak": 3, "points": 4700}, ...}
+        self.answered = 0
+        self.correct = 0
+        self.score = 0
+        self.curr_streak = 0
+        self.max_streak = 0
+        self.streak_reset = False
+
+    def add_correct(self):
+        if self.streak_reset:
+            self.streak_reset = False
+        self.answered += 1
+        self.correct += 1
+        self.score += 1000 + (self.curr_streak * 50)
+        self.curr_streak += 1
+        if self.curr_streak > self.max_streak:
+            self.max_streak = self.curr_streak
+
+    def add_incorrect(self):
+        if self.streak_reset:
+            self.streak_reset = False
+        self.answered += 1
+        self.score -= 250
+        if self.curr_streak > 1:
+            self.streak_reset = True
+        self.curr_streak = 0
+
+
+class QuizData:
+    def __init__(self, message: Message, allowed_emotes: list):
+        self.message = message
+        self.allowed_emotes = allowed_emotes
+        self.current_answers = {}  # {35: "\U0001F1E6", 58: "\U0001F1E8", ...}
+        self.player_data = {}
+
+    def _add_user(self, user_id):
+        self.player_data[user_id] = ScoreData()
+        return self.player_data[user_id]
+
+    def set_answer(self, user_id, emote):
+        if emote in self.allowed_emotes:
+            self.current_answers[user_id] = emote
+
+    def update_scores(self, correct_emote):
+        correct_users = []
+        incorrect_users = []
+
+        for user_id, emote in list(self.current_answers.items()):  # The list cast returns a copy, so it can't update.
+            if emote == correct_emote:
+                self.add_correct(user_id)
+                correct_users.append(user_id)
+            else:
+                self.add_incorrect(user_id)
+                incorrect_users.append(user_id)
+
+        self.current_answers = {}  # Reset the chosen answers
+        return correct_users, incorrect_users
+
+    def add_correct(self, user_id):
+        player_data: ScoreData = self.player_data.get(user_id, None)
+        if player_data is None:
+            print("No player data")
+            player_data = self._add_user(user_id)
+        player_data.add_correct()
+
+    def add_incorrect(self, user_id):
+        player_data: ScoreData = self.player_data.get(user_id, None)
+        if player_data is None:
+            print("No player data")
+            player_data = self._add_user(user_id)
+        player_data.add_incorrect()
+
+    def top_scores(self, max_num=None):
+        data_by_score = sorted(self.player_data.items(), key=lambda t: t[1].score, reverse=True)
+        if max_num is None:
+            return data_by_score
+        num = min(max_num, len(data_by_score))
+        return data_by_score[:num]
+
+
 class Quiz(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.session = None
-        self.active_quiz = None
+        self.option_emojis = ["🇦", "🇧", "🇨", "🇩"]  # Note: These are regional indicator emojis
+        self.active_quiz_data = {}  # {[svr_id]: QuizData}
 
-    async def cog_before_invoke(self, ctx):
-        self.session = ClientSession()
-        # Check if module enabled
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction: Reaction, user):
+        if user.bot:
+            return
+        guild_quiz_data: QuizData = self.active_quiz_data.get(reaction.message.guild.id, None)
 
-    async def cog_after_invoke(self, ctx):
-        await self.session.close()
+        # Checks if a quiz is running, and that this is the correct message
+        if guild_quiz_data is None:
+            return
+        elif guild_quiz_data.message.id != reaction.message.id:
+            return
+        print(reaction.emoji)
 
-    async def get_json_content(self, url):
-        async with self.session.get(url) as resp:
-            print("Response:" + str(resp.status))
-            json_string = await resp.text()
-        return loads(json_string)
+        guild_quiz_data.set_answer(user.id, reaction.emoji)  # change emote
+        await reaction.remove(user)
 
     @commands.command()
     @commands.max_concurrency(1, per=commands.BucketType.guild)
     async def quiz(self, ctx, rounds: int = 5):
-        if rounds > 50:
-            await ctx.channel.send("The maximum number of questions in one quiz is 50.")  # Technically 150 is the limit
+        if rounds > 25 or rounds < 1:
+            await ctx.channel.send("You can have between 1-25 questions per quiz.")  # Technically 150 is the limit
             return
 
         # Tries to fetch equal number of questions for each difficulty. Remainder precedence Medium, Hard, Easy
         questions = []
+        quotient, remainder = divmod(rounds, 3)
         for diff in ["easy", "medium", "hard"]:
-            diff_amount = rounds // 3
-            if diff == "medium" and rounds % 3 != 0:  # remainder 1 or 2
+            diff_amount = quotient
+            if (diff == "medium" and remainder != 0) or (diff == "hard" and remainder == 2):
                 diff_amount += 1
-            if diff == "hard" and rounds % 3 == 2:  # remainder 2
-                diff_amount += 1
-            url = f"https://opentdb.com/api.php?amount={diff_amount}&type=multiple&difficulty={diff}"
-            data = await self.get_json_content(url)
-            for question in data['results']:
-                questions.append(question)
+            questions += await get_questions(diff, diff_amount)
         print(questions)
-        # Note that there's a disproportionately large number of questions in the 'Entertainment: Video Games' category
 
-        players = {}
+        # Note: Disproportionately large number of questions in the 'Entertainment: Video Games' category
+
         em = Embed(
-            title="A Quiz will start in 15 seconds.",
+            title="A Quiz will start in 10 seconds.",
             description=(
-                "- A series of multiple choice questions will be displayed here.\n"
-                "- Add one of the **A, B, C or D** reactions to answer within the time limit.\n"
-                "- Correct answers get **+1000** points, but incorrect ones get **-250**.\n"
+                "A series of multiple choice questions will be displayed here.\n"
+                "Add one of the **A, B, C or D** reactions to answer within the time limit.\n\n"
+                "🔹 Correct answers gain **1000** points\n"
+                "🔸 Incorrect ones lose **250** points\n"
+                "🔥 Get an answer **streak** for bonus points\n\n"
                 "Good Luck!"
             )
         )
         em.set_footer(text=f"This Quiz will have {rounds} questions | Sourced from the OpenTDB API")
-        q_message = await ctx.channel.send(embed=em)
-        self.active_quiz = q_message.jump_url
+        quiz_msg: Message = await ctx.channel.send(embed=em)
 
-        ordered_players = []
-        await sleep(5)
+        quiz_data = QuizData(quiz_msg, self.option_emojis)
+        self.active_quiz_data[ctx.guild.id] = quiz_data
+
+        await sleep(10)
         for question_no in range(rounds):
-            q_data = questions[question_no]
+            question = questions[question_no]
 
-            em = Embed(colour=0x8B008B, title=unescape(q_data['question']))
+            em = Embed(colour=0xFA8072, title=unescape(question['question']))
             em.set_footer(text=f"Question {question_no + 1} of {rounds} | {15} seconds per question")
-            em.set_author(name=f"{q_data['difficulty'].title()} | {q_data['category']}",
-                          icon_url=ctx.me.avatar_url)
+            em.set_author(
+                name=f"{question['difficulty'].title()} | {question['category']}",
+                icon_url=ctx.me.avatar_url
+            )
 
-            options = q_data['incorrect_answers']
-            options.append(q_data['correct_answer'])
+            options = question['incorrect_answers']
+            options.append(question['correct_answer'])
             shuffle(options)
 
-            letter_blocks = ["\U0001F1E6", "\U0001F1E7", "\U0001F1E8", "\U0001F1E9"]
-            corr_index = 0
+            overwrite = PermissionOverwrite(add_reactions=False)
+            await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+
+            correct_index = 0
             for i in range(len(options)):
-                em.add_field(name=letter_blocks[i], value=unescape(options[i]), inline=False)
-                if options[i] == q_data['correct_answer']:
-                    corr_index = i
-                await q_message.add_reaction(letter_blocks[i])
+                em.add_field(name=self.option_emojis[i], value=unescape(options[i]), inline=False)
+                if options[i] == question['correct_answer']:
+                    correct_index = i
+                await quiz_msg.add_reaction(self.option_emojis[i])
+
+            overwrite = PermissionOverwrite(add_reactions=None)
+            await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+
             for i in range(3):
-                await q_message.edit(content=f":alarm_clock: You have {15 - (i * 5)} Seconds to answer this question", embed=em)
+                await quiz_msg.edit(
+                    content=f":alarm_clock: You have {15 - (i * 5)} Seconds to answer this question",
+                    embed=em
+                )
                 await sleep(5)
-            await q_message.edit(content="\n:alarm_clock: Time's up!")
+            await quiz_msg.edit(content="\n:alarm_clock: Time's up!")
 
-            corr_users = set()
-            incorr_users = set()
+            correct_ids, incorrect_ids = quiz_data.update_scores(self.option_emojis[correct_index])
+            correct_mentions = [ctx.guild.get_member(user_id).mention for user_id in correct_ids]
+            incorrect_mentions = [ctx.guild.get_member(user_id).mention for user_id in incorrect_ids]
+            if len(correct_mentions) == 0:
+                correct_text = "Looks like no-one got it right this round!"
+            else:
+                correct_text = " ".join(correct_mentions) + " | Correct!"
+            if len(incorrect_mentions) == 0:
+                incorrect_text = "No incorrect answers."
+            else:
+                incorrect_text = " ".join(incorrect_mentions) + " | Incorrect..."
+
             # Get message by id again, as reaction data is not updated
-            q_message = await ctx.channel.fetch_message(q_message.id)
+            quiz_msg = await ctx.channel.fetch_message(quiz_msg.id)
+            await quiz_msg.clear_reactions()
 
-            for i in range(len(q_message.reactions)):
-                reaction = q_message.reactions[i]
-                if reaction.emoji in letter_blocks:
-                    if i == corr_index:
-                        corr_users = set(await reaction.users().flatten()) - {ctx.me}
-                    else:
-                        async for elem in reaction.users():
-                            if elem != ctx.me:
-                                incorr_users.add(elem)
-
-            await q_message.clear_reactions()
-
-            for user in (corr_users | incorr_users):
-                if user.id not in players:
-                    players[user.id] = {"score": 0, "correct": 0, "incorrect": 0}
-
-            corr_results = ""
-            for user in (corr_users - incorr_users):
-                corr_results += (user.mention + " ")
-                players[user.id]["score"] += 1000
-                players[user.id]["correct"] += 1
-
-            if corr_results != "":
-                corr_text = corr_results + "| Correct!"
-            else:
-                corr_text = "Looks like no-one got it right this round!"
-
-            incorr_results = ""
-            for user in incorr_users:
-                incorr_results += (user.mention + " ")
-                players[user.id]["score"] -= 250
-                players[user.id]["incorrect"] += 1
-
-            if incorr_results != "":
-                incorr_text = incorr_results + "| Incorrect..."
-            else:
-                incorr_text = "No Incorrect Answers."
-            # TODO: Tie reaction emojis to what's counted, not just the position.
-            # TODO: Remove the need for this, if possible
-            # Turns each {'id': {'item1': 0, 'item2': 0},} into [('id',{'item1': 0, 'item2': 0}),]
-            ordered_players = sorted(players.items(), key=lambda k: k[1]["score"], reverse=True)
-            value = ""
-            for num in range(len(ordered_players)):
-                player = ordered_players[num]
-                user = await self.bot.fetch_user(player[0])
-                value += f"#{num + 1} | {user.mention} | {player[1]['score']}\n"
+            # returns a list of tuples [(id: int, sd: ScoreData)], we can iterate with 'for key, value in ...'
+            top_ten = quiz_data.top_scores(10)
+            pos = 1
+            slots = []
+            for user_id, score_data in top_ten:
+                member = ctx.guild.get_member(user_id)
+                slot = f"#{pos} | {member.mention} | {score_data.score} "
+                if score_data.curr_streak > 1:
+                    slot += f" **🔥 {score_data.curr_streak}**"
+                elif score_data.streak_reset:
+                    slot += f" **🧯 0**"
+                slots.append(slot)
             em = Embed(
-                title=f"Answer | **{letter_blocks[corr_index]} {unescape(q_data['correct_answer'])}**",
-                description=f"{corr_text}\n{incorr_text}"
+                title=f"Answer | **{self.option_emojis[correct_index]} {unescape(question['correct_answer'])}**",
+                description=f"{correct_text}\n{incorrect_text}"
             )
-            em.add_field(name="Current Standings:", value=value)
+            em.add_field(
+                name="Current Standings:",
+                value="\n".join(slots) or "Noone's answered yet :("
+            )
             em.set_footer(text=f"{rounds - question_no - 1} questions remain | Advancing in 5 seconds")
-            await q_message.edit(embed=em)
+            await quiz_msg.edit(embed=em)
             await sleep(5)
 
-        value = ""
-        for num in range(len(ordered_players)):
-            player = ordered_players[num]
-            user = await self.bot.fetch_user(player[0])
-            value += f"#{num+1} | {user.mention} | {player[1]['correct']}/{player[1]['incorrect']} | {player[1]['score']}\n"
-        em = Embed(title="Final Scores", colour=0x8B008B, description=value)
-        em.set_footer(text=f"Lasted {rounds} rounds.")
-        em.set_author(name="Quiz Complete | Type '6.quiz' for another one!", icon_url=ctx.me.avatar_url)
-        await q_message.edit(embed=em)
+        # Final standings
+        pos = 1
+        slots = []
+        url = None
+        for user_id, score_data in quiz_data.player_data.items():
+            member = ctx.guild.get_member(user_id)
+            if pos == 1:
+                url = str(member.avatar_url)
+
+            slot = f"#{pos} | {member.mention} | {score_data.correct}/{score_data.answered} | {score_data.score} "
+            if score_data.max_streak > 1:
+                slot += f"- **🔥 {score_data.max_streak}** Max"
+            slots.append(slot)
+
+        em = Embed(title="Final Scores", colour=0xFA8072, description="\n".join(slots) or "Hello? Anyone there?")
+        em.set_footer(text=f"Lasted {rounds} rounds | Type '6.quiz' to play again")
+        em.set_author(name="Quiz Complete | Thanks for playing!", icon_url=ctx.me.avatar_url)
+        if url is not None:
+            em.set_thumbnail(url=url)
+
+        await quiz_msg.edit(embed=em)
 
     @quiz.error
     async def quiz_error(self, ctx, error):
         print(error)
         if isinstance(error, commands.MaxConcurrencyReached):
+            message = self.active_quiz_data[ctx.guild.id].message
             await ctx.send(f"Only one quiz can be active at once. "
-                           f"You can find the current game here:\n{self.active_quiz}")
+                           f"You can find the current game here:\n{message.jump_url}", delete_after=15.0)
         elif isinstance(error, commands.UserInputError):
             await ctx.send("Make sure to specify a *positive number* of rounds.")
 
